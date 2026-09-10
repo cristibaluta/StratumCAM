5aeaa4b9e19d02a600545007a8a1f2b39eec02b0
Cristian Baluta <cristi.baluta@gmail.com>
2026-09-11 00:17:55 +0300

    Fix model init

--- Sources/StratumCAM/Models.swift
+++ Sources/StratumCAM/Models.swift
@@ -39,10 +39,21 @@ public enum SC {
         public struct Chained: Sendable {
             public var entity: DXF.Entity
             public var reversed: Bool
+
+            // The inits are needed because the app using the lib is not able to init otherwise
+            public init(entity: DXF.Entity, reversed: Bool) {
+                self.entity = entity
+                self.reversed = reversed
+            }
         }
 
         public var entities: [Chained]
         public var isClosed: Bool
+
+        public init(entities: [Chained], isClosed: Bool) {
+            self.entities = entities
+            self.isClosed = isClosed
+        }
     }
 
     // MARK: - 2. Tooling Configuration
